528352ffd26b22887cfd2cd3f1a3e6055059ad5b
Klaus Leggewie <klaus.leggewie@syzygy.de>
2022-05-24 16:04:45 +0200

    convert to new minimal hosting model

--- GraphQlWebCore/Program.cs
+++ GraphQlWebCore/Program.cs
@@ -1,17 +1,59 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using GraphQL.Server;
+using GraphQL.Types;
+using GraphQlTypes.Schemas;
+using GraphQlWebCore.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace GraphQlWebCore
-{
-    public static class Program
+
+var builder = WebApplication.CreateBuilder(args);
+
+// add services to the container.
+builder.Services.AddMvc(); // required for showing razor view after start
+builder.Services.AddMvcCore();
+builder.Services.AddRegistration();
+
+// Add GraphQL services and configure options
+builder.Services.AddGraphQL
+    (options =>
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+        options.EnableMetrics = builder.Environment.IsDevelopment();
+        options.UnhandledExceptionDelegate = ctx => Console.WriteLine(ctx.OriginalException.Message);
+    })
+    .AddSystemTextJson()
+    .AddDataLoader()
+    .AddGraphTypes(typeof(EmployeeSchema));
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+// kestrel
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.AllowSynchronousIO = true;
+});
+
+var app = builder.Build();
+
+// configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseStaticFiles();
+app.UseRouting();
+
+// the route is just for the intro page - it is not required for the graphql service
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+});
+
+// use HTTP middleware for Schema at path /graphql
+app.UseGraphQL<ISchema>("/graphql");
+
+// use GraphQL Playground middleware at default path /ui/playground with default options
+app.UseGraphQLPlayground();
+
+app.Run();
